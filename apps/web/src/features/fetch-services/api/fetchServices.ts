a9500fdd8483688aa9cfcef87192bfb161335a19
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-04-12 22:31:07 +0900

    仮の値を返すモックの関数を作成

---
+++ apps/web/src/features/fetch-services/api/fetchServices.ts
@@ -0,0 +1,12 @@
+import { ServiceModel } from "@/entities/service"
+
+const serviceListMock = [
+    {
+        title: "Proxmox VE",
+        address: "https://192.168.0.200:8006"
+    }
+] as const satisfies ServiceModel[];
+
+export const fetchServices = async () => {
+    return serviceListMock;
+}
