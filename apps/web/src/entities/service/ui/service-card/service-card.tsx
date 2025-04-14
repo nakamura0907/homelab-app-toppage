89687907b4fb8cee5b7e1fc381780953c91158a2
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-04-14 22:46:28 +0900

    サービスカードコンポーネントでカードコンポーネントを呼び出すように変更

--- apps/web/src/entities/service/ui/service-card/service-card.tsx
+++ apps/web/src/entities/service/ui/service-card/service-card.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { ServiceModel } from "../../types";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 type Props = {
     model: ServiceModel
 };
 
-export const ServiceCard: React.FC<Props> = () => {
+export const ServiceCard: React.FC<Props> = ({ model }) => {
     return (
-        <div></div>
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    <Link href={model.address} target="_blank" rel="noopener noreferrer">
+                        {model.title}
+                    </Link>
+                </CardTitle>
+            </CardHeader>
+        </Card>
     )
 }
