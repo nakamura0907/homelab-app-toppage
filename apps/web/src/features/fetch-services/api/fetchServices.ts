393e127ab826ff17f8c548df11afe87b1903e908
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-04-20 01:16:19 +0900

    モックのサービス一覧に要素を追加

--- apps/web/src/features/fetch-services/api/fetchServices.ts
+++ apps/web/src/features/fetch-services/api/fetchServices.ts
@@ -5,6 +5,18 @@ const serviceListMock = [
         title: 'Proxmox VE',
         address: 'https://192.168.0.200:8006',
     },
+    {
+        title: 'Pi-hole',
+        address: 'http://192.168.0.231'
+    },
+    {
+        title: 'Prometheus',
+        address: 'http://192.168.0.232:9090'
+    },
+    {
+        title: 'Loki',
+        address: 'http://192.168.0.233:3100'
+    }
 ] as const satisfies ServiceModel[];
 
 export const fetchServices = async () => {
