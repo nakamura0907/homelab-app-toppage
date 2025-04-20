7bb4648e4871f0446e2fb301094946ed6c8d02ad
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-04-20 12:43:12 +0900

    テスト関係のパッケージインストール

--- apps/web/jest.config.mjs
+++ apps/web/jest.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('jest').Config} */
 export default {
     preset: 'ts-jest',
-    testEnvironment: 'node',
+    testEnvironment: 'jsdom',
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
     },
