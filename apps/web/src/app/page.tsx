5f9a6a55916bc826b8ead3cd531502bcbb4a1d43
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-04-23 08:53:57 +0900

    サービス登録ダイアログコンポーネントを呼び出し

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,13 @@
 import { DraggableServiceList } from '@/features/draggable-service-list';
 import { fetchServices } from '@/features/fetch-services';
+import { RegisterServiceDialog } from '@/features/register-service';
 
 export default async function Home() {
     const services = await fetchServices();
-    return <DraggableServiceList services={services} />;
+    return (
+        <div className='container mx-auto p-4'>
+            <RegisterServiceDialog />
+            <DraggableServiceList services={services} />
+        </div>
+    )
 }
