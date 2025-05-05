6a65810428dab2f6514ecce3393973abae82f087
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-05-06 00:14:41 +0900

    Resultクラスの実装

---
+++ apps/web/src/shared/helpers/result/result.ts
@@ -0,0 +1,71 @@
+/**
+ * Resultクラス
+ * 
+ * @see https://medium.com/@cole.carley/implementing-the-rust-result-type-in-typescript-2c2a93c0c165
+ */
+export class Result<T, E extends Error> {
+    private readonly _ok: T | null;
+    private readonly _err: E | null;
+
+    /**
+     * Resultクラスのコンストラクタ
+     * 
+     * @throws {Error} ok, errの両方に値がない。と判定された場合
+     * @throws {Error} ok, errの両方に値がある。と判定された場合
+     */
+    constructor(ok: T | null, err: E | null) {
+        // 両方に値がない場合
+        if (!ok && !err) {
+            throw new Error('Result must have a value or an error');
+        }
+        // 両方に値がある場合
+        if (ok && err) {
+            throw new Error('Result cannot have both a value and and error')
+        }
+
+        // okに値がある場合
+        if (ok !== null) {
+            this._ok = ok;
+            this._err = null;
+
+            // errに値がある場合
+        } else {
+            this._ok = null;
+            this._err = err;
+        }
+    }
+
+    static ok<T>(value: T) {
+        return new Result(value, null);
+    }
+
+    static err<E extends Error>(error: E) {
+        return new Result(null, error);
+    }
+
+    /**
+     * 成功した場合は結果を返し、エラーの場合は例外をスローする
+     * 
+     * @throws {E} 結果がエラーの場合
+     * @throws {Error} 何らかの理由で成功もエラーもしていないと判定された場合
+     */
+    unwrap(): T {
+        if (this.isOk()) {
+            return this._ok as T;
+        }
+
+        if (this.isErr()) {
+            throw this._err as E;
+        }
+
+        throw new Error('Unknown error');
+    }
+
+    isOk(): this is Result<T, never> {
+        return this._ok !== null;
+    }
+
+    isErr(): this is Result<never, E> {
+        return this._err !== null;
+    }
+}
