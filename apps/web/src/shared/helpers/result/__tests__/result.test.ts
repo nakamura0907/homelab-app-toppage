885f7550b10df07a8ef06c3ed2a766e23af05756
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-05-06 00:26:16 +0900

    Resultクラスのテストを作成

---
+++ apps/web/src/shared/helpers/result/__tests__/result.test.ts
@@ -0,0 +1,56 @@
+import { Result } from "../result";
+
+describe("Result", () => {
+    const value = "OK";
+    const err = new Error("test");
+
+    describe('成功したResult', () => {
+        it('Resultクラスをインスタンス化する', () => {
+            expect(() => { Result.ok(value) }).not.toThrow();
+            expect(() => { new Result(value, null) }).not.toThrow();
+        })
+        it('成功と失敗を判定する', () => {
+            const result = Result.ok(value);
+
+            expect(result.isOk()).toBeTruthy();
+            expect(result.isErr()).toBeFalsy();
+        })
+        it('中身の値を取得する', () => {
+            const result = Result.ok(value);
+
+            expect(result.unwrap()).toBe(value);
+        })
+    })
+
+    describe('失敗したResult', () => {
+        it('Resultクラスをインスタンス化する', () => {
+            expect(() => { Result.err(err) }).not.toThrow();
+            expect(() => { new Result(null, err) }).not.toThrow();
+        })
+        it('成功と失敗を判定する', () => {
+            const result = Result.err(err);
+
+            expect(result.isOk()).toBeFalsy();
+            expect(result.isErr()).toBeTruthy();
+        })
+        it('中身の値を取得する', () => {
+            const result = Result.err(err);
+
+            expect(() => result.unwrap()).toThrow(err);
+        })
+    })
+
+    describe('成功と失敗の値が渡されたResult', () => {
+        it('Resultクラスをインスタンス化する', () => {
+            expect(() => { new Result(value, err) }).toThrow();
+        })
+    })
+
+    describe('成功の値も失敗の値も渡されなかったResult', () => {
+        it('Resultクラスをインスタンス化する', () => {
+            expect(() => { Result.ok(null) }).toThrow();
+            // expect(() => { Result.err(null) }).toThrow();
+            expect(() => { new Result(null, null) }).toThrow();
+        })
+    })
+})
