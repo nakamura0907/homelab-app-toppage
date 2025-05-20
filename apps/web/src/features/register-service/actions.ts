609c52cf7c65c3e661098464b50e8f31086a77ff
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-05-20 23:20:46 +0900

    Server ActionsでResultクラスを返却しないように修正

--- apps/web/src/features/register-service/actions.ts
+++ apps/web/src/features/register-service/actions.ts
@@ -1,27 +1,19 @@
 'use server';
 
 import { serviceClientFactory } from '@/entities/service';
-import { Result } from '@/shared';
 import { revalidatePath } from 'next/cache';
 
 /**
  * サービスを登録するServer Action
  */
 export async function registerService(formData: FormData) {
-    try {
-        const client = serviceClientFactory();
+    const client = serviceClientFactory();
 
-        const title = formData.get('title') as string;
-        const address = formData.get('address') as string;
+    const title = formData.get('title') as string;
+    const address = formData.get('address') as string;
 
-        await client.registerService({ title, address });
+    await client.registerService({ title, address });
 
-        // ページを再検証
-        revalidatePath('/');
-
-        return Result.ok({});
-    } catch {
-        const err = new Error('サービスの登録に失敗しました');
-        return Result.err(err);
-    }
+    // ページを再検証
+    revalidatePath('/');
 }
