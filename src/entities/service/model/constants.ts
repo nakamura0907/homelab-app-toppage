fce7d21d6ebf25a8098170a9f5c8088ecdec2f1a
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-06-12 21:19:43 +0900

    トップページの作成 #9

--- src/entities/service/model/constants.ts
+++ src/entities/service/model/constants.ts
@@ -3,4 +3,21 @@ import { ServiceModel } from './type';
 /**
  * サーバーのサービス一覧
  */
-export const services = [] as const satisfies ServiceModel[];
+export const services = [
+  {
+    title: 'Proxmox',
+    address: 'https://192.168.0.200:8006/',
+  },
+  {
+    title: 'Pi-hole',
+    address: 'http://192.168.0.231/',
+  },
+  {
+    title: 'Grafana',
+    address: 'http://192.168.0.241:3000/',
+  },
+  {
+    title: 'Prometheus',
+    address: 'http://192.168.0.241:9090/',
+  },
+] as const satisfies ServiceModel[];
